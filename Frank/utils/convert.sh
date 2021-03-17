4339d143e6b3480d44c90ea76e8b73a42f754978
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2021-03-17 21:40:57 +0100

    convert zip to gz

---
+++ Frank/utils/convert.sh
@@ -0,0 +1,11 @@
+unzip -p meteo.zip | gzip -c > meteo.csv.gz
+unzip -p opzetstukken.zip | gzip -c > opzetstukken.csv.gz
+unzip -p strain-group1.zip | gzip -c > strain-group1.csv.gz
+unzip -p strain-group2.zip | gzip -c > strain-group2.csv.gz
+unzip -p strain-group3.zip | gzip -c > strain-group3.csv.gz
+unzip -p strain-group4.zip | gzip -c > strain-group4.csv.gz
+unzip -p strain-group5.zip | gzip -c > strain-group5.csv.gz
+unzip -p strain-group6.zip | gzip -c > strain-group6.csv.gz
+unzip -p strain-group7.zip | gzip -c > strain-group7.csv.gz
+unzip -p strain-group8.zip | gzip -c > strain-group8.csv.gz
+unzip -p strain-temp.zip | gzip -c > strain-temp.csv.gz
