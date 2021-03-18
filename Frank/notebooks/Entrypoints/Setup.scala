e87eafacb2c0695321105518f47722376c8901b8
frankivo <frankivo>
2021-03-18 16:32:51 +0000

    setup silver

--- Frank/notebooks/Entrypoints/Setup.scala
+++ Frank/notebooks/Entrypoints/Setup.scala
@@ -1,3 +1,8 @@
 // Databricks notebook source
 // MAGIC %sql
 // MAGIC create database if not exists biobridge_bronze location "/mnt/biobridge/bronze.db"
+
+// COMMAND ----------
+
+// MAGIC %sql
+// MAGIC create database if not exists biobridge_silver location "/mnt/biobridge/silver.db"
