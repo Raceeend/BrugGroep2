c7f4d0ee931a6784f556d8053c8dda09ab809150
frankivo <frankivo>
2021-03-18 17:01:12 +0000

    runner

---
+++ Frank/notebooks/2_Silver/0_runner.scala
@@ -0,0 +1,10 @@
+// Databricks notebook source
+dbutils.notebook.run("./meteo", 0, Map())
+
+// COMMAND ----------
+
+dbutils.notebook.run("./opzetstukken", 0, Map())
+
+// COMMAND ----------
+
+dbutils.notebook.run("./strain-group", 0, Map())
