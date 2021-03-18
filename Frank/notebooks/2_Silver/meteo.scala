cee6a98b2c97c3d4a62afa2a26522768464425c4
frankivo <frankivo>
2021-03-18 16:39:42 +0000

    cast fields

---
+++ Frank/notebooks/2_Silver/meteo.scala
@@ -0,0 +1,18 @@
+// Databricks notebook source
+import org.apache.spark.sql.functions.{to_timestamp, translate}
+
+spark.table("biobridge_bronze.meteo")
+  .where($"Datum-tijd".isNotNull) // Filter extra header
+  .select(
+    to_timestamp($"Datum-tijd").as("Datum-tijd"),
+    translate($"Temp", ",", ".").cast("Double").as("Temp"),
+    translate($"Windsnelheid", ",", ".").cast("Double").as("Windsnelheid"),
+    translate($"Windrichting", ",", ".").cast("Double").as("Windrichting"),
+    translate($"Luchtvochtigheid", ",", ".").cast("Double").as("Luchtvochtigheid"),
+    translate($"Luchtdruk", ",", ".").cast("Double").as("Luchtdruk"),
+    translate($"Neerslag", ",", ".").cast("Double").as("Neerslag"),
+    translate($"Zonneschijn", ",", ".").cast("Double").as("Zonneschijn")
+  )
+  .write
+  .mode("overwrite")
+  .saveAsTable("biobridge_silver.meteo")
